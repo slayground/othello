c64c878adf474bcf4a83dc4f7aa21a7685cce434
huynet <cocwedc@gmail.com>
2019-04-13 22:54:06 -0500

    call it a day

--- BoardGame.java
+++ BoardGame.java
@@ -2,7 +2,7 @@ class BoardGame {
     public static void main(String[] args) {
         System.out.println("Boardgame sucks");
         Othello othello = new Othello();
-        othello.possibleMoves('W');
+        othello.possibleMoves('B');
 
         othello.displayPossibleMoves();
     }
